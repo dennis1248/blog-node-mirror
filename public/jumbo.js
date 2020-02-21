5063851260416ae1da7bf63d3946ce6526271950
Dennis ten Hoove <dennistenhoove@protonmail.com>
2020-02-21 14:03:05 +0100

    Add jumbotron

---
+++ public/jumbo.js
@@ -0,0 +1,31 @@
+const jumboLeft = document.getElementById("jumbo_btn_left");
+const jumboRight = document.getElementById("jumbo_btn_right")
+const jumboBody = document.getElementById("main_jumbo");
+
+var img = 1;
+var imgCount = 3; // The amount of images it needs to load
+
+function rollImgFoward() {
+  if (img === (imgCount + 1)) {
+    img = 1;
+  }
+}
+
+function rollImgbackwards() {
+  if (img === 0) {
+    img = imgCount;
+  }
+}
+
+jumboLeft.addEventListener("click", function () {
+  img--;
+  rollImgbackwards()
+  jumboBody.style.backgroundImage = "url(/images/placeholder_" + img + ".jpg)";
+});
+
+jumboRight.addEventListener("click", function () {
+  img++;
+  rollImgFoward();
+  jumboBody.style.backgroundImage = "url(/images/placeholder_" + img + ".jpg)";
+  console.log(img);
+});
