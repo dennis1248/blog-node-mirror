6a7ecd0a5de87c97841dfb0b9435b7ce7969aa48
Dennis ten Hoove <dennistenhoove@protonmail.com>
2020-02-25 13:39:43 +0100

    load posts from database

--- app.js
+++ app.js
@@ -1,22 +1,70 @@
 // Server config
-const express = require("express");
-const app = express();
+const express     = require("express"),
+      app         = express(),
+      mongoose    = require("mongoose"),
+      bodyParser  = require("body-parser");
+
 
 // Define folder for static files
 app.use(express.static("public"));
 
-// port
+// Test data
+var today = new Date(),
+    dd = String(today.getDate()).padStart(2, '0'),
+    mm = String(today.getMonth() + 1).padStart(2, '0'),
+    yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+
+var userName = "Dennis1248";
+var title = "How to be cool";
+var post = "Potatos are very cool ayy lmao lol something not every interesting"
+
+// Mongoose
+mongoose.connect("mongodb://localhost/blog");
+
+var blogPostSchema = new mongoose.Schema ({
+  name: String,
+  date: String,
+  title: String,
+  post: String
+});
+
+var blogPost = mongoose.model("blogPost", blogPostSchema);
+
+blogPost.create({
+  name: userName,
+  date: today,
+  title: title,
+  post: post
+}, function (err, blogPost) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("New database entry");
+    console.log(blogPost);
+  }
+})
+
+// Server config
 const port = 8081;
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Home page
 app.get("/", function (req, res) {
   res.render("home.ejs");
 })
 
-
 // Posts page
 app.get("/posts", function (req, res) {
-  res.render("posts.ejs");
+  blogPost.find({}, function (err, blogPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("posts.ejs", {blogPost:blogPost});
+    }
+  })
 })
 
 // About page
@@ -29,7 +77,7 @@ app.get("/login", function (req, res) {
   res.render("login.ejs");
 })
 
-// Login page
+// Login
 app.post("/login", function (req, res) {
   // Put logic here
   res.redirect("/");
